29d727aafe1502efa173b9262347f8cc120b762d
dvan <ph_van@hotmail.com>
2011-03-08 16:04:49 +0100

    dpvdepot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '9324d1ac6954bf02f82f23d1c93f1e785147cd8fdf5077a8405c245674a61d510ad71a8fa4b18a26a114a9354fa1819db13b427a2a07ef01b393240386dd9947'
